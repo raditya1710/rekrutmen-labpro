546e9175239ee5e1e5196880905fcfc4c25102e3
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2016-08-07 18:58:02 +0700

    Adding another activity

--- HMIFPIOrganizer/app/src/main/java/com/hmif/raditya1710/hmifpiorganizer/ShowActivity.java
+++ HMIFPIOrganizer/app/src/main/java/com/hmif/raditya1710/hmifpiorganizer/ShowActivity.java
@@ -3,19 +3,13 @@ package com.hmif.raditya1710.hmifpiorganizer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ShowActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void gotoShow(View view){
-        Intent intent = new Intent(this, ShowActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_show);
     }
 
     @Override
